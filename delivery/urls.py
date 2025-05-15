8d1ebf6a5570a8dfc77912c1223c20ea14241478
tejashxv <tejashyadavcr7@gmail.com>
2025-05-15 22:46:16 +0530

    integrated realtime location tracker using api

--- delivery/urls.py
+++ delivery/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home.views import index, get_data
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('get_data/', get_data, name='get_data'),
+   
 ]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import LocationUpdate
+from django.http import JsonResponse
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def get_data(request):
+    latest_data = LocationUpdate.objects.latest('timestamp')
+    return JsonResponse({
+        'latitude': latest_data.latitude,
+        'longitude': latest_data.longitude,
+        'timestamp': latest_data.timestamp,
+    })
